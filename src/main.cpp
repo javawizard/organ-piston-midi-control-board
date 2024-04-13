44ba493c76b18c642dc64a671924b31e8478e27d
Alex Boyd <alex@opengroove.org>
2024-04-13 00:00:59 -0600

    IT LIVES!!!!!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#include "esp32-hal-tinyusb.h"
+#include <Adafruit_TinyUSB.h>
+#include <MIDI.h>
 
 const auto I2C_FREQUENCY = 50'000ul;
 const auto DEBOUNCE_TIME = 30'000ll; // in microseconds
@@ -26,6 +27,9 @@ int64_t turn_led_off_at = -1;
 int64_t loop_count = 0;
 
 Adafruit_NeoPixel esp_board_led(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+Adafruit_USBD_MIDI usb_midi;
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 
 class Piston {
   public:
@@ -52,10 +56,15 @@ class Piston {
     }
 
     void send_midi_event(uint8_t midi_channel, uint8_t midi_note, bool current_state, uint32_t led_color) {
-      if (tud_midi_mounted()) {
+      if (TinyUSBDevice.mounted()) {
         // lots of narrowing conversion warnings here; it turns out the | operator promotes its operands to ints. grr. oh well. could static_cast our way out of the warning but whatever.
-        uint8_t packet[] = {(current_state ? MIDI_NOTE_ON : MIDI_NOTE_OFF) | midi_channel, midi_note, (current_state ? uint8_t{127} : uint8_t{0})};
-        tud_midi_stream_write(0, packet, sizeof(packet));
+        // uint8_t packet[] = {(current_state ? MIDI_NOTE_ON : MIDI_NOTE_OFF) | midi_channel, midi_note, (current_state ? uint8_t{127} : uint8_t{0})};
+        // tud_midi_stream_write(0, packet, sizeof(packet));
+        if (current_state) {
+          MIDI.sendNoteOn(midi_note, 127, midi_channel);
+        } else {
+          MIDI.sendNoteOff(midi_note, 0, midi_channel);
+        }
       }
 
       esp_board_led.fill(led_color);
@@ -73,7 +82,7 @@ class Keyboard {
       address0(address0),
       address1(address1),
       midi_channel(midi_channel),
-      midi_offset(midi_offset),
+      midi_note_offset(midi_note_offset),
       led_color(led_color)
     {}
 
@@ -104,7 +113,7 @@ class Keyboard {
       uint32_t pin_states = (expander1.readGPIOAB() << 16) | expander0.readGPIOAB();
 
       for (int i = 0; i < PINS_PER_KEYBOARD; i++) {
-        pistons[i].update_with_new_value(midi_channel, midi_offset + i, !(pin_states & 1), led_color);
+        pistons[i].update_with_new_value(midi_channel, midi_note_offset + i, !(pin_states & 1), led_color);
         pin_states = pin_states >> 1;
       }
     }
@@ -112,7 +121,7 @@ class Keyboard {
     uint8_t address0;
     uint8_t address1;
     uint8_t midi_channel;
-    uint8_t midi_offset;
+    uint8_t midi_note_offset;
     uint32_t led_color;
 
     Adafruit_MCP23X17 expander0;
@@ -122,43 +131,56 @@ class Keyboard {
 };
 
 Keyboard keyboards[] = {
-  Keyboard(0x20, 0x21, 0, 0, 0x0000FF), // choir
-  Keyboard(0x22, 0x23, 0, 32, 0x00FF00), // great
-  // Keyboard(0x24, 0x25, 0, 64, 0xFFFF00), // swell
-  // Keyboard(0x26, 0x27, 0, 96, 0xFF0000), // solo
-  Keyboard(0x60, 0x61, 1, 0, 0x00FFFF), // pedal left (translated through an LTC4316 on the pedal controller)
-  // Keyboard(0x62, 0x63, 1, 32, 0xFF00FF) // pedal right (translated through an LTC4316 on the pedal controller)
+  Keyboard(0x20, 0x21, 1, 0, 0x0000FF), // choir
+  Keyboard(0x22, 0x23, 1, 32, 0x00FF00), // great
+  // Keyboard(0x24, 0x25, 1, 64, 0xFFFF00), // swell
+  // Keyboard(0x26, 0x27, 1, 96, 0xFF0000), // solo
+  Keyboard(0x60, 0x61, 2, 0, 0x00FFFF), // pedal left (translated through an LTC4316 on the pedal controller)
+  // Keyboard(0x62, 0x63, 2, 32, 0xFF00FF) // pedal right (translated through an LTC4316 on the pedal controller)
 };
 
-extern "C" uint16_t load_midi_usb_descriptor(uint8_t *dst, uint8_t *itf) {
-  uint8_t str_index = tinyusb_add_string_descriptor("Organ Piston MIDI Controller (by javawizard weeeeee)");
-  uint8_t ep_num = tinyusb_get_free_duplex_endpoint();
-  TU_VERIFY(ep_num != 0);
-  uint8_t descriptor[TUD_MIDI_DESC_LEN] = {
-      // Interface number, string index, EP Out & EP In address, EP size
-      TUD_MIDI_DESCRIPTOR(*itf, str_index, ep_num, (uint8_t)(0x80 | ep_num),
-                          64)};
-  *itf += 1;
-  memcpy(dst, descriptor, TUD_MIDI_DESC_LEN);
-  return TUD_MIDI_DESC_LEN;
-}
+// extern "C" uint16_t load_midi_usb_descriptor(uint8_t *dst, uint8_t *itf) {
+//   uint8_t str_index = tinyusb_add_string_descriptor("Organ Piston MIDI Controller (by javawizard weeeeee)");
+//   uint8_t ep_num = tinyusb_get_free_duplex_endpoint();
+//   TU_VERIFY(ep_num != 0);
+//   uint8_t descriptor[TUD_MIDI_DESC_LEN] = {
+//       // Interface number, string index, EP Out & EP In address, EP size
+//       TUD_MIDI_DESCRIPTOR(*itf, str_index, ep_num, (uint8_t)(0x80 | ep_num),
+//                           64)};
+//   *itf += 1;
+//   memcpy(dst, descriptor, TUD_MIDI_DESC_LEN);
+//   return TUD_MIDI_DESC_LEN;
+// }
 
-void sink_midi_events() {
-  // Read off and discard any MIDI events the host has sent us. We don't use these but they'll queue up and cause
-  // whatever's connected to the port on the USB host side to hang if we don't read them off.
-  uint8_t packet[4];
+// void sink_midi_events() {
+//   // Read off and discard any MIDI events the host has sent us. We don't use these but they'll queue up and cause
+//   // whatever's connected to the port on the USB host side to hang if we don't read them off.
+//   uint8_t packet[4];
 
-  while (tud_midi_available()) {
-    tud_midi_packet_read(reinterpret_cast<uint8_t*>(&packet));
+//   while (tud_midi_available()) {
+//     tud_midi_packet_read(reinterpret_cast<uint8_t*>(&packet));
+//   }
+// }
+
+void flash_error_and_restart(uint8_t flashes) {
+  for (int i = 0; i < flashes; i++) {
+    esp_board_led.fill(0xFF0000);
+    esp_board_led.show();
+    delay(250);
+    esp_board_led.fill(0);
+    esp_board_led.show();
+    delay(250);
   }
+
+  delay(15000); // wait 15 seconds; needed so that we don't restart too quickly when soft entering DFU mode and kick back to regular mode
+  ESP.restart();
 }
 
 void setup() {
-  USB.productName("Organ Piston MIDI Controller (by javawizard weeeeee)");
-  USB.manufacturerName("github.com/javawizard");
-  USB.VID(0x1234);
-  tinyusb_enable_interface(USB_INTERFACE_MIDI, TUD_MIDI_DESC_LEN, load_midi_usb_descriptor);
-  USB.begin();
+  USBDevice.setProductDescriptor("Organ Piston MIDI Controller");
+  USBDevice.setManufacturerDescriptor("github.com/javawizard");
+  usb_midi.setStringDescriptor("Organ Piston MIDI Port");
+  MIDI.begin(MIDI_CHANNEL_OMNI);
 
   delay(1000);
 
@@ -176,16 +198,7 @@ void setup() {
     // (Also see if it ever fails to come up and, if it does, do that for the sake of robustness as well)
     if (!keyboards[i].initialize()) {
       // Setup failed; flash to indicate which expander can't be found, then reboot and try again.
-      for (size_t flash = 0; flash <= i; flash++) {
-        esp_board_led.fill(0xFF0000);
-        esp_board_led.show();
-        delay(250);
-        esp_board_led.fill(0);
-        esp_board_led.show();
-        delay(250);
-      }
-      delay(15000); // wait 15 seconds; needed so that we don't restart too quickly when soft entering DFU mode and kick back to regular mode
-      ESP.restart();
+      flash_error_and_restart(i);
     }
   }
 
